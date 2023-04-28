ba50857dd363c365efa6ae9982e25293399695c1
cena <cena.pakdel@gmail.com>
2023-04-28 10:09:28 +0330

    fix minor MovieEntity

--- app/src/main/java/ir/sina/moviemaktab/model/entity/MovieEntity.kt
+++ app/src/main/java/ir/sina/moviemaktab/model/entity/MovieEntity.kt
@@ -1,11 +1,18 @@
 package ir.sina.moviemaktab.model.entity
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "movie_table")
 data class MovieEntity(
+    @PrimaryKey(autoGenerate = false)
+    @ColumnInfo("movie_id")
     val id: Int,
+    @ColumnInfo("title")
     val title: String,
-    val releaseDate: String,
+    @ColumnInfo("poster_path")
     val posterPath: String,
+    @ColumnInfo("release_date")
+    val releaseDate: String,
 )
