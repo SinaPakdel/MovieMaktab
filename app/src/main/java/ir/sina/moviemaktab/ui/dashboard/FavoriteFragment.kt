11cab3f1832a81ea2547e27f58823df8f08df3d4
cena <cena.pakdel@gmail.com>
2023-04-28 12:32:21 +0330

    modify some ui

--- app/src/main/java/ir/sina/moviemaktab/ui/dashboard/FavoriteFragment.kt
+++ app/src/main/java/ir/sina/moviemaktab/ui/dashboard/FavoriteFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import ir.sina.moviemaktab.databinding.FragmentDashboardBinding
+import ir.sina.moviemaktab.databinding.FragmentFavoriteBinding
 
-class DashboardFragment : Fragment() {
+class FavoriteFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentFavoriteBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        val favoriteViewModel =
+            ViewModelProvider(this).get(FavoriteViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentFavoriteBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
+        favoriteViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
