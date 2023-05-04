1865070ce8c401f5a5cf1c43354e77e82e0dee7c
sina <cena.pakdel@gmail.com>
2023-05-04 11:05:02 +0330

    change name

--- app/src/main/java/ir/sina/moviemaktab/model/dto/MoviesResponseDto.kt
+++ app/src/main/java/ir/sina/moviemaktab/model/dto/MoviesResponseDto.kt
@@ -3,11 +3,11 @@ package ir.sina.moviemaktab.model.dto
 
 import com.google.gson.annotations.SerializedName
 
-data class MoviesDto(
+data class MoviesResponseDto(
     @SerializedName("page")
     val page: Int,
     @SerializedName("results")
-    val movies: List<MovieDto>,
+    val result: List<MovieDto>,
     @SerializedName("total_pages")
     val totalPages: Int,
     @SerializedName("total_results")
