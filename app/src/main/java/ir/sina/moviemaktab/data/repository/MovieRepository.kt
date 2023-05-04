ceac5d60372753c21304e8fce24265920c997a31
sina <cena.pakdel@gmail.com>
2023-05-04 10:12:46 +0330

    impl repository with new responseState

--- app/src/main/java/ir/sina/moviemaktab/data/repository/MovieRepository.kt
+++ app/src/main/java/ir/sina/moviemaktab/data/repository/MovieRepository.kt
@@ -4,6 +4,7 @@ import ir.sina.moviemaktab.data.local.db.MovieDao
 import ir.sina.moviemaktab.data.remote.MovieApiService
 import ir.sina.moviemaktab.model.ui.MovieItem
 import ir.sina.moviemaktab.util.Mapper
+import ir.sina.moviemaktab.util.ResponseState
 import ir.sina.moviemaktab.util.asMovieEntity
 import javax.inject.Inject
 
@@ -11,10 +12,22 @@ class MovieRepository @Inject constructor(
     private val movieApiService: MovieApiService,
     private val movieDao: MovieDao
 ) {
-    suspend fun getMovieList(page: Int): List<MovieItem> {
-        val movieListResponse = movieApiService.getAllMovies(page)
-        val movieEntityList = movieListResponse.map { movieDto -> movieDto.asMovieEntity() }
-        movieDao.insertAll(movieEntityList)
-        return Mapper.movieEntityToItem(movieEntityList)
+    suspend fun getMovieList(page: Int): ResponseState<List<MovieItem>> {
+        return try {
+
+            val movieListResponse = movieApiService.getAllMovies(page)
+            val movieEntityList =
+                movieListResponse.map { movieDto -> movieDto.asMovieEntity() }
+            movieDao.insertAll(movieEntityList)
+
+            val success =
+                ResponseState.Success( Mapper.movieEntityToItem(movieEntityList))
+
+            success
+        } catch (e: Exception) {
+            return ResponseState.Error(e.message.toString())
+        }
+
+
     }
 }
